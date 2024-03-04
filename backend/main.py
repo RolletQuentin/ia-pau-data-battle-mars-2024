d7958b359a1d674728d937aa6ed134d8f7c7577d
RolletQuentin <rolletquen@gmail.com>
2024-03-04 23:35:30 +0100

    write SQL query to get all data about study cases

--- backend/main.py
+++ backend/main.py
@@ -66,3 +66,77 @@ async def get_all_data():
     except Exception as e:
         # If a problem occurs, return a code 500 error (Internal Server Error)
         raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get("/get_study_cases")
+async def get_all_data():
+    try:
+        # Create cursor object
+        cursor = mydb.cursor()
+
+        # Execute the query
+        query = """
+            SELECT
+                tblrex.numrex,
+                tblrex.codemonnaie,
+                tblrex.gainfinancierrex,
+                tblrex.gainfinancierperioderex,
+                tblrex.energierex,
+                tblrex.codeuniteenergie,
+                tblrex.codeperiodeenergie,
+                tblrex.codeenergierex,
+                tblrex.gesrex,
+                tblrex.ratiogainrex,
+                tblrex.trirex,
+                tblrex.capexrex,
+                tblrex.capexperioderex,
+                tblrex.codeTechno1,
+                tblrex.codeTechno2,
+                tblrex.codeTechno3,
+                tblrex.codetravaux,
+                tblrex.codereseau,
+                tblregion.codepays,
+                tblreference.coderegion,
+                tblreference.codesecteur,
+                tblcoutrex.reelcoutrex,
+                tblcoutrex.codemonnaiecoutrex,
+                tblcoutrex.codedifficulte,
+                tblgainrex.gainfinanciergainrex,
+                tblgainrex.codemonnaiegainrex,
+                tblgainrex.codeperiodeeconomie,
+                tblgainrex.energiegainrex,
+                tblgainrex.uniteenergiegainrex,
+                tblgainrex.codeperiodeenergie,
+                tblgainrex.gesgainrex,
+                tblgainrex.trireelgainrex
+            FROM tblrex
+            JOIN tblreference ON tblreference.numreference = tblrex.codereference
+            JOIN tblregion ON tblregion.numregion = tblreference.coderegion
+            JOIN tblcoutrex ON tblcoutrex.coderex = tblrex.numrex
+            JOIN tblgainrex ON tblgainrex.coderex = tblrex.numrex
+            LIMIT 100;
+        """
+        cursor.execute(query)
+
+        # Get the results
+        results = cursor.fetchall()
+
+        # Close the cursor
+        cursor.close()
+
+        # Make a great JSON
+        datas = []
+        names = ["numsolution", "fichetechno"]
+        for result in results:
+            data = {}
+            for i in range(len(names)):
+                data[names[i]] = result[i]
+
+            datas.append(data)
+
+        # Return datas
+        return datas
+
+    except Exception as e:
+        # If a problem occurs, return a code 500 error (Internal Server Error)
+        raise HTTPException(status_code=500, detail=str(e))
