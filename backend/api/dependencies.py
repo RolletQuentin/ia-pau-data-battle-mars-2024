fba811395a9508da73bea85b5b6adf61b9220211
RolletQuentin <rolletquen@gmail.com>
2024-03-25 12:42:46 +0100

    fix divided by 0 standard deviation

--- backend/api/dependencies.py
+++ backend/api/dependencies.py
@@ -11,13 +11,21 @@ mydb = mysql.connector.connect(
 
 
 def weighted_mean(data):
+
     # Calculate the mean and standard deviation of the data
     mean = np.mean(data)
     std_dev = np.std(data)
+
+    # Check if the standard deviation is zero
+    if std_dev == 0:
+        return mean  # Return the mean directly if all values are the same
+
     # Calculate the weights from the normal distribution
     weights = norm.pdf(data, mean, std_dev)
+
     # Normalize the weights
     weights /= np.sum(weights)
+
     # Calculate the weighted average
     weighted_avg = np.sum(data * weights)
     return weighted_avg
