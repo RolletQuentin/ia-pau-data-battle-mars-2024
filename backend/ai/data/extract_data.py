f7f12740bb56dea2d8180c4eec1a26101f001f07
VERGNET-MarcAntoine <mc.ate.vgt@gmail.com>
2024-03-27 14:27:01 +0100

    le csv solution modification

--- backend/ai/data/extract_data.py
+++ backend/ai/data/extract_data.py
@@ -18,43 +18,6 @@ def clean(text):
     return text
 
 
-def extractDictionnary(sql_path,output_file,element,codes):
-    # Obtention du chemin absolu du répertoire contenant le script
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-
-    # Construction du chemin d'accès au fichier SQL relatif à l'emplacement du script
-    sql_file_path = os.path.join(script_dir, sql_path)
-    output_file_path = os.path.join(script_dir, output_file)
-
-    # Conversion de la liste des codes en une chaîne pour l'expression régulière
-    codes_pattern = '|'.join(map(str, codes))
-
-    # Expression régulière pour extraire les lignes qui correspondent à vos critères
-    pattern = re.compile(rf",2,'{element}',(.{{1,4}}?),(?:{codes_pattern})," )
-
-    # Ouverture et traitement du fichier SQL
-    with open(sql_file_path, 'r') as file,  open(output_file_path, 'w',encoding='utf-8') as output:
-        for line in file:
-            # Divise la ligne en plusieurs lignes si elle contient "),("
-            split_lines = re.split(r'\),\(', line)
-            for sub_line in split_lines:
-                # Vérifie si la sous-ligne correspond au motif
-                if pattern.search(sub_line):
-                    # Séparation des colonnes par des virgules
-                    columns = sub_line.split(',',5)                 
-                    columns[5] = re.sub('<.*?>', '',columns[5])
-                    columns[5] = re.sub(r"\\'", "'", columns[5])  # Remplace \' par '
-                    columns[5] = re.sub(r"\\n", " ", columns[5])  # Remplace \n par un espace
-                    columns[5] = re.sub(r"\\r", " ", columns[5])  # Remplace \r par un espace
-                    columns[5] = re.sub(r'\\"', '"', columns[5])  # Remplace \" par "
-                    columns[5] = re.sub(r"^'", '', columns[5])  # Supprime l'apostrophe au début de la chaîne
-                    columns[5] = re.sub(r"'$", '', columns[5])  # Supprime l'apostrophe à la fin de la chaîne
-                    columns[5] = re.sub(r'\s+', ' ', columns[5]).strip()  # Nettoie les espaces multiples et enlève les espaces de début et de fin
-
-                    # Écriture des colonnes autres que les trois premières dans le fichier CSV
-                    output.write('|'.join(columns[3:]) + '\n')
-
-
 def getSolutions():
     mydb = mysql.connector.connect(
         host="localhost",
@@ -86,8 +49,7 @@ def getSolutions():
         WHERE 
             dic.codelangue = 2 AND 
             dic.typedictionnaire = 'sol' AND
-            sol.validsolution = 1 AND
-            dic.indexdictionnaire IN (1, 2, 5, 6, 9, 10, 11, 12)
+            dic.indexdictionnaire IN (1, 2, 5, 6)
         ORDER BY 
             sol.numsolution;
     """
@@ -143,7 +105,8 @@ def mapSolutionsToTech():
     query = f"""
         SELECT numsolution,
             validsolution 
-        FROM mydatabase.tblsolution;
+        FROM mydatabase.tblsolution
+        Where validsolution = 0;
     """
 
     cursor.execute(query)
@@ -158,5 +121,5 @@ def mapSolutionsToTech():
     cursor.close()
     mydb.close()
 
-mapSolutionsToTech()
 
+mapSolutionsToTech()
